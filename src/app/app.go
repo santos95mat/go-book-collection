c30919bc28fc899a97ab448c5a44112fea7a4e1b
santos95mat <santos95.mat@gmail.com>
2023-10-18 11:37:21 -0300

    feat: create book routes

--- src/app/app.go
+++ src/app/app.go
@@ -5,16 +5,24 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/santos95mat/book-collection/src/app/routes"
 )
 
 var app = fiber.New()
+var v1 = app.Group("/v1")
 
 func Run() {
 	app.Use(cors.New())
+
+	getRoutes()
 
 	app.Get("/status", func(c *fiber.Ctx) error {
 		return c.SendString("Book-collection API is running")
 	})
 
 	app.Listen(os.Getenv("PORT"))
+}
+
+func getRoutes() {
+	routes.AddBookRoutes(v1)
 }
