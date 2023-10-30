7afd965e30c85114539d478520c32e6cc717cd31
santos95mat <santos95.mat@gmail.com>
2023-10-30 11:20:49 -0300

    fix: remove SearchBookDto from bookDto

--- src/dto/bookDto.go
+++ src/dto/bookDto.go
@@ -6,10 +6,3 @@ type BodyBookDto struct {
 	Gender string `json:"gender"`
 	Year   string `json:"year"`
 }
-
-type SearchBookDto struct {
-	Name   string `query:"name"`
-	Author string `query:"author"`
-	Gender string `query:"gender"`
-	Year   string `query:"year"`
-}
