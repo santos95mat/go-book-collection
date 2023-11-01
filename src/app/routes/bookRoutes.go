fb083b566f863c807744ff65fbd6b2e9a4e223d8
santos95mat <santos95.mat@gmail.com>
2023-11-01 12:31:10 -0300

    change: add auth middleware in bock and user route

--- src/app/routes/bookRoutes.go
+++ src/app/routes/bookRoutes.go
@@ -3,12 +3,13 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/santos95mat/go-book-collection/src/controller"
+	"github.com/santos95mat/go-book-collection/src/middleware"
 )
 
 var bookController controller.BookController
 
 func AddBookRoutes(v1 fiber.Router) {
-	book := v1.Group("/book")
+	book := v1.Group("/book", middleware.Auth)
 
 	book.Post("/", bookController.Create)
 
--- src/app/routes/userRoutes.go
+++ src/app/routes/userRoutes.go
@@ -3,21 +3,25 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/santos95mat/go-book-collection/src/controller"
+	"github.com/santos95mat/go-book-collection/src/middleware"
 )
 
 var userController controller.UserController
 
 func AddUserRoutes(v1 fiber.Router) {
 	user := v1.Group("/user")
+	login := v1.Group("/login")
 
 	user.Post("/", userController.Create)
 
-	user.Get("/", userController.GetMany)
+	user.Get("/", middleware.Auth, userController.GetMany)
 
-	user.Get("/:id", userController.GetOne)
+	user.Get("/:id", middleware.Auth, userController.GetOne)
 
-	user.Put("/:id", userController.Update)
+	user.Put("/:id", middleware.Auth, userController.Update)
 
-	user.Delete("/:id", userController.Delete)
+	user.Delete("/:id", middleware.Auth, userController.Delete)
+
+	login.Post("/", userController.Login)
 
 }
