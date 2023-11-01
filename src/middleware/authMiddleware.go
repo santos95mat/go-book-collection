34e461491e556359134c4556d925f0acf4df1345
santos95mat <santos95.mat@gmail.com>
2023-11-01 18:03:35 -0300

    fix: organized middleware code

--- src/middleware/authMiddleware.go
+++ src/middleware/authMiddleware.go
@@ -10,18 +10,23 @@ import (
 	"github.com/santos95mat/go-book-collection/src/model"
 )
 
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+	if !ok {
+		return nil, nil
+	}
+
+	return []byte(os.Getenv("SECRET")), nil
+}
+
 func Auth(c *fiber.Ctx) error {
 	tokenStr := c.Cookies("Authorization")
 
-	token, _ := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, nil
-		}
+	token, _ := jwt.Parse(tokenStr, keyFunc)
 
-		return []byte(os.Getenv("SECRET")), nil
-	})
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
 		if float64(time.Now().Unix()) > claims["exp"].(float64) {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 				"message": "Unauthorized",
